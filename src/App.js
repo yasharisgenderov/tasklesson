3a3f7c586f9f89b2356640558b0cfa9e68afa38e
yasharisgenderov <yasharisgenderov.0@gmail.com>
2022-12-05 20:39:00 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  let c = 0;
   return (
     <div className="App">
       <div className="counter">
         <button
           onClick={() => {
-            count < -10 ? setCount(count - 1) : count;
+            setCount(count - 1);
           }}
         >
           -
         </button>
-        {console.log(count)}
         <div className="count">{count}</div>
         <button
           onClick={() => {
-            count < 10 ? setCount(count + 1) : count;
+            setCount(count + 1);
           }}
         >
           +
